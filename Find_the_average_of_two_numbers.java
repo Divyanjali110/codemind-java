89ef26f80d0abb824081d8d1545574caecbed491
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2024-01-06 10:50:41 +0530

    import java.util.Scanner; public class Average{          public static void main(String[] args)           {
                   Scanner sc= new Scanner(System.in);
                  float a = sc.nextFloat(); //to read an integer
                  float b = sc.nextFloat();
                  float d=((a+b)/2);//to read an integer
                 System.out.printf("%.4f",d);
               }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Average{
+         public static void main(String[] args)
+          {
+                
+               Scanner sc= new Scanner(System.in);
+              float a = sc.nextFloat(); //to read an integer
+              float b = sc.nextFloat();
+              float d=((a+b)/2);//to read an integer
+             System.out.printf("%.4f",d);
+           }
+}
