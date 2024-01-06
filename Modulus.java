fc660fb6bf53eefa53eb49fab644c0cd6757da04
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2024-01-06 09:51:48 +0530

    import java.util.Scanner; public class Modulus{          public static void main(String[] args)           {
                   Scanner sc= new Scanner(System.in);
                  int a = sc.nextInt(); //to read an integer
                  int b = sc.nextInt(); //to read an integer
                 System.out.printf("%d",(a%b));
               }
    }

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Modulus{
+         public static void main(String[] args)
+          {
+                 
+               Scanner sc= new Scanner(System.in);
+              int a = sc.nextInt(); //to read an integer
+              int b = sc.nextInt(); //to read an integer
+             System.out.printf("%d",(a%b));
+           }
+}
