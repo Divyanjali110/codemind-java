67bdf114110711bc84582322711bb74515cd7494
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2024-01-08 11:20:39 +0530

    import java.util.Scanner; public class SecondNumber{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);
            int x=sc.nextInt();
            int y=sc.nextInt();
            int s=(2*x)-y;
            System.out.printf("%d",s);
           
        }
    }

---
+++ Find_Second_Number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SecondNumber{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int s=(2*x)-y;
+        System.out.printf("%d",s);
+       
+    }
+}
+
+
