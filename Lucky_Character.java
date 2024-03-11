c860a1e7b7e163deccb126d1a784a9901eca7ba2
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2024-03-11 21:51:02 +0530

    import java.util.*; public class LuckyCharacter {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.next();         if(s.length()<=10)         {             System.out.println(s.charAt(6));         }     } }

---
+++ Lucky_Character.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class LuckyCharacter
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        if(s.length()<=10)
+        {
+            System.out.println(s.charAt(6));
+        }
+    }
+}
