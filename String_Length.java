86f9b2342a80f817b0c51140200924975cc96bb9
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2024-03-11 22:02:32 +0530

    import java.util.*; public class StringLength {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.next();
                System.out.println(s.length());
            
            
        }
    }

---
+++ String_Length.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class StringLength
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        
+            System.out.println(s.length());
+        
+        
+    }
+}
