c0ac7c022b50eddbb0b96b1d04fbadc9db5854d9
Divyanjali110 <119731145+Divyanjali110@users.noreply.github.com>
2024-03-11 22:00:58 +0530

    import java.util.*; public class StringCopy {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         String s1Copy=s1;
                System.out.println(s1Copy);
            
            
        }
    }

---
+++ String_Copy.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class StringCopy
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s1Copy=s1;
+        
+            System.out.println(s1Copy);
+        
+        
+    }
+}
